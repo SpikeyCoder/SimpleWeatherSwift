3cc0ac81126cca1df8d6f990658f97f2d114bfe7
Kevin Armstrong <karmstron21@gmail.com>
2016-01-07 11:00:16 -0500

    Adding data source for breaking out the view controller

---
+++ WXControllerDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  WXControllerDataSource.swift
+//  SimpleWeather
+//
+//  Created by Kevin Armstrong on 1/7/16.
+//  Copyright © 2016 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+class WXControllerDataSource: NSObject {
+
+}
