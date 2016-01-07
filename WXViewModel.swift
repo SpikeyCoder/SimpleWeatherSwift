b0b9292714f0f0354e2e0ffedaf53db6461d3370
Kevin Armstrong <karmstron21@gmail.com>
2016-01-07 09:28:16 -0500

    first swift files

---
+++ WXViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WXViewModel.swift
+//  SimpleWeather
+//
+//  Created by Kevin Armstrong on 1/7/16.
+//  Copyright © 2016 Ryan Nystrom. All rights reserved.
+//
+
+import UIKit
+
+class WXViewModel: NSObject {
+
+}
